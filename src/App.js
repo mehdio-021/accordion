8a235d9382c220d66d13e03e25dcfe5a9ee1f105
mehdi <mehdii.moharami@gmail.com>
2023-05-14 12:37:04 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Question from "./Question";
+import data from "./data";
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>سوالی برایتان پیش آمده؟؟</h3>
+      <div className="info">
+        {questions.map((question) => {
+          return <Question key={question.id} {...question}/>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+
+
+const Question = ({title,info}) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <button onClick={()=>setShowInfo(!showInfo)}>
+          {showInfo ?<AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </div>
+  );
+};
+
+export default Question;
